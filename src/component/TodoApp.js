f909ee20be2fb62f63d31e27191627b6c337a43d
aashiashfak <aashiashfak@gmail.com>
2024-05-16 13:39:02 +0530

    completed edit icon and functionality

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -5,6 +5,8 @@ class TodoApp extends Component {
   state = {
     input: "",
     items: [],
+    editingIndex: null, // Index of the item being edited
+    editedValue: "", // Value of the item being edited
   };
 
   handleChange = (event) => {
@@ -17,22 +19,55 @@ class TodoApp extends Component {
     event.preventDefault();
     const {input} = this.state;
 
+    if (input.trim() !== "") {
+      this.setState({
+        items: [...this.state.items, input],
+        input: "",
+      });
+    }
+
+    console.log(this.state.items);
+  };
+
+  deleteData = (key) => {
     this.setState({
-      items: [...this.state.items, input],
-      input: "",
+      items: this.state.items.filter((data, index) => index !== key),
     });
     console.log(this.state.items);
   };
 
-  deleteData = (key)=>{
+  editItem = (index) => {
+    const {items} = this.state;
+    const edited = items[index]; // Get the value of the item being edited
+
     this.setState({
-      items:this.state.items.filter((data,index)=> index!== key)
-    })
-    console.log(this.state.items)
-  }
+      editingIndex: index,
+      editedValue:edited,
+    });
+  };
+
+  cancelEdit = () => {
+    this.setState({
+      editingIndex: null, // Reset editing state
+      editedValue: "", // Reset edited value
+    });
+  };
+
+  updateItem = () => {
+    const {items, editingIndex, editedValue} = this.state;
+
+    // Replace the old value with the edited value
+    items[editingIndex] = editedValue;
+
+    this.setState({
+      items,
+      editingIndex: null, // Reset editing state
+      editedValue: "", // Reset edited value
+    });
+  };
 
   render() {
-    const {input, items} = this.state;
+    const {input, items, editingIndex, editedValue} = this.state;
     return (
       <form>
         <div className="todo-container">
@@ -50,14 +85,37 @@ class TodoApp extends Component {
         <ul>
           {items.map((data, index) => (
             <li key={index}>
-              {data}
-              <div>
-                <i
-                  onClick={() => this.deleteData(index)}
-                  id="delete-icon"
-                  className="fa-solid fa-trash-can"
-                ></i>
-              </div>
+              {editingIndex === index ? ( // Render input field if editing
+                <>
+                  <input
+                    type="text"
+                    value={editedValue}
+                    onChange={(e) =>
+                      this.setState({editedValue: e.target.value})
+                    }
+                  />
+                  <div>
+                    <button onClick={this.cancelEdit}>Cancel</button>
+                    <button onClick={this.updateItem}>Update</button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <span>{data}</span>
+                  <div>
+                    <i
+                      onClick={() => this.deleteData(index)}
+                      id="delete-icon"
+                      className="fa-solid fa-trash-can"
+                    ></i>
+                    <i
+                      onClick={() => this.editItem(index)}
+                      id="edit-icon"
+                      className="fa-solid fa-pen-to-square"
+                    ></i>
+                  </div>
+                </>
+              )}
             </li>
           ))}
         </ul>
