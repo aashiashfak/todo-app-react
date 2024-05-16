0f0dd76f2b8fd1e60b1ebd91169c84e5325510e0
aashiashfak <aashiashfak@gmail.com>
2024-05-16 11:37:22 +0530

    item published

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -2,18 +2,56 @@ import React, {Component} from "react";
 import "./TodoApp.css";
 
 class TodoApp extends Component {
+  state = {
+    input: "",
+    items:[]
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value,
+    });
+    console.log(this.state.input)
+  };
+
+  storeItems = (event) => {
+    event.preventDefault();
+    const {input} = this.state
+
+    this.setState({
+      items: [...this.state.items, input],
+      input:""
+    })
+    console.log(this.state.items)
+  };
+
   render() {
+    const {input,items} = this.state;
     return (
-      <div className="todo-container">
-        <h1>Todo App</h1>
-        <div className="input-section">
-          <input type="text" />
-          <button>Add</button>
+      <form>
+        <div className="todo-container">
+          <h1>Todo App</h1>
+          <div className="input-section">
+            <input
+              type="text"
+              value={input}
+              onChange={this.handleChange}
+              placeholder="Enter Items..."
+            />
+            <button onClick={this.storeItems}>Add</button>
+          </div>
         </div>
         <ul>
-            <li>items</li>
+          {items.map((data, index) => (
+            <li key={index}>
+              {data}
+              <div>
+                <i id="delete-icon" className="fa-solid fa-trash-can"></i>
+              </div>
+            </li>
+          ))}
         </ul>
-      </div>
+      </form>
     );
   }
 }
